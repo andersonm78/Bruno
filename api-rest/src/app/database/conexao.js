280336b5ddabdd9799328a6a01991d531bf844b1
andersonm78 <163211042+andersonm78@users.noreply.github.com>
2025-09-05 21:01:36 -0300

    Add files via upload
    Subindo projeto aula-5

---
+++ api-rest/src/app/database/conexao.js
@@ -0,0 +1,15 @@
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: '',
+    database: 'cursosdb',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+
+/* Aplicamos o export para utilizar o objeto em outro local */
+export default pool;
